93f08db510145b207596b0f779423ddd42696170
SamuelBajracharya <linuxsmuffproductivity@gmail.com>
2025-07-15 11:59:55 +0545

    WIP: made to, from and fromTo methods return a promise

--- src/scripts/gsap.js
+++ src/scripts/gsap.js
@@ -1,31 +1,83 @@
 import setVariables from "./setVariables.js";
 
 export class gsapClone {
-
-    #apply(selector, properties, prefix = '') {
+    #animationPromise(selector, properties, prefix = '') {
         const elements = document.querySelectorAll(selector);
-        elements.forEach((element) => {
-            element.classList.add(`gsap-${prefix.replace(/-$/, '')}`);
-            setVariables(element, properties, prefix);
+        return new Promise((resolve) => {
+            let elementCount = elements.length;
+            if (elementCount === 0) {
+                resolve();
+                return;
+            }
+            elements.forEach((element) => {
+                element.classList.add(`gsap-${prefix.replace(/-$/, '')}`);
+                setVariables(element, properties, prefix);
+                const onAnimationEnd = (event) => {
+                    if (event.target === element) {
+                        element.removeEventListener("animationend", onAnimationEnd);
+                        element.removeEventListener("transitionend", onAnimationEnd);
+                        elementCount--;
+                        if (elementCount === 0) {
+                            resolve();
+                        }
+                    }
+                }
+                element.addEventListener("animationend", onAnimationEnd);
+                element.addEventListener("transitionend", onAnimationEnd);
+
+                setTimeout(() => {
+                    if (elementCount > 0) {
+                        elementCount = 0;
+                        resolve();
+                    }
+                }, 5000);
+            })
         })
     }
 
     to(selector, properties) {
-        this.#apply(selector, properties, 'to-');
+        return this.#animationPromise(selector, properties, 'to-');
     }
 
     from(selector, properties) {
-        this.#apply(selector, properties, 'from-');
+        return this.#animationPromise(selector, properties, 'from-');
     }
 
     fromTo(selector, fromToProperties) {
         const elements = document.querySelectorAll(selector);
-        elements.forEach((element) => {
-            element.classList.add('gsap-fromTo');
-            setVariables(element, fromToProperties["from"], 'from-');
-            setVariables(element, fromToProperties["to"], 'to-');
-            setVariables(element, fromToProperties, 'fromTo-');
-        });
+        return new Promise((resolve) => {
+            let elementCount = elements.length;
+            if (elementCount === 0) {
+                resolve();
+                return;
+            }
+            elements.forEach((element) => {
+                element.classList.add('gsap-fromTo');
+                setVariables(element, fromToProperties["from"], 'from-');
+                setVariables(element, fromToProperties["to"], 'to-');
+                setVariables(element, fromToProperties, 'fromTo-');
+
+                const onAnimationEnd = (event) => {
+                    if(event.target === element){
+                        element.removeEventListener("animationend", onAnimationEnd);
+                        element.removeEventListener("transitionend", onAnimationEnd);
+                        elementCount--;
+                        if(elementCount === 0){
+                            resolve();
+                        }
+                    }
+                }
+                element.addEventListener("animationend", onAnimationEnd);
+                element.addEventListener("transitionend", onAnimationEnd);
+
+                setTimeout(() => {
+                    if(elementCount > 0){
+                        elementCount = 0;
+                        resolve();
+                    }
+                }, 5000);
+            })
+        })
     }
 
 }
